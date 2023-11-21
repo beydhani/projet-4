a0f6c939a09a1e2fedc6ce6c5920f0064887d5a9
orleans2k17 <orleans2k17@gmail.com>
2023-11-21 20:41:40 +0000

    Second commit

---
+++ Blog/models/Administrateur.php
@@ -0,0 +1,8 @@
+<?php
+// Administrateur.php
+// C'est le modèle qui va communiquer avec notre base de données pour la table administrateur.
+// Il va gérer les données et les renvoyer au controleur AdministrateurController.php
+
+
+//On requiert le fichier pour se connecter à la base de données.
+require_once (APP_ROOT .'/config/database.php');
