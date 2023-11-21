c83332e9f2ce53064c34ad8b68a24fa4a08f2929
orleans2k17 <orleans2k17@gmail.com>
2023-11-21 20:49:41 +0000

    Méthode connecter() de Administrateur.php

--- Blog/models/Administrateur.php
+++ Blog/models/Administrateur.php
@@ -18,4 +18,33 @@ class Administrateur {
     public function __construct($db) {
         $this->conn = $db; //
     }
+    // Méthode connecter
+    public function connecter() {
+        // Requête SQL pour vérifier les informations de l'administrateur
+        $query = "SELECT id, nom_utilisateur, mot_de_passe FROM " . $this->table_name . " WHERE nom_utilisateur = :nom_utilisateur LIMIT 0,1";
+
+        // Préparation de la requète avec PDO (méthode prepare de pdo)
+        $stmt = $this->conn->prepare($query);
+
+        // Nettoyage du nom d'utilisateur fourni pour éviter les injections SQL et les failles XSS
+        $this->nom_utilisateur = htmlspecialchars(strip_tags($this->nom_utilisateur));
+        //On lie le nom d'utilisateur fourni au paramètre de la requète avec bindParam
+        $stmt->bindParam(':nom_utilisateur', $this->nom_utilisateur);
+
+        // Exécution de la requête
+        $stmt->execute();
+
+        // On récupère la ligne de résultat sous forme de tableau associatif
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // On vérifie le mot de passe fourni avec le mot de passe dans le tableau récupéré
+        //  avec la méthode password_verify
+        //Si c'est bon on assigne l'id à notre objet et on retourne true
+        if (password_verify($this->mot_de_passe, $row['mot_de_passe'])) {
+            $this->id = $row['id'];
+            return true;
+        }
+        // Sinon on retourne false
+        return false;
+    }
 }
