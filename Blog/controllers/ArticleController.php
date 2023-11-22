092f1a8cf658bfa460b33777c9bc857100944738
orleans2k17 <orleans2k17@gmail.com>
2023-11-22 11:09:59 +0000

    Création de la méthode pour lire tous les articles

--- Blog/controllers/ArticleController.php
+++ Blog/controllers/ArticleController.php
@@ -18,5 +18,13 @@ class ArticleController {
         $this->db = $database->getConnection();
         $this->articleModel = new Article($this->db);
     }
+    // Afficher tous les articles
+    public function tousArticles() {
+        ob_start();
+        $resultat = $this->articleModel->lire();
+        require_once APP_ROOT.'/views/TousArticles.php';
+        $contenu = ob_get_clean();
+        echo $contenu;
+    }
 }
 ?>
