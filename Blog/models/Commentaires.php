5a58eb730cd5948816d08c92733a81ada8f72ed8
orleans2k17 <orleans2k17@gmail.com>
2023-11-22 10:29:23 +0000

    Méthode pour ajouter un nouveau commentaire

--- Blog/models/Commentaires.php
+++ Blog/models/Commentaires.php
@@ -18,5 +18,30 @@ class Commentaire {
     public function __construct($db) {
         $this->conn = $db;
     }
+
+    // Méthode pour ajouter un nouveau commentaire
+    public function ajouter() {
+        // Requête SQL 
+        $query = "INSERT INTO " . $this->table_name . " SET id_article=:id_article, id_utilisateur=:id_utilisateur, contenu=:contenu, date_publication=NOW()";
+
+        // Préparation de la requête
+        $stmt = $this->conn->prepare($query);
+
+        // Nettoyage et liaison des données
+        $this->id_article = htmlspecialchars(strip_tags($this->id_article));
+        $this->id_utilisateur = htmlspecialchars(strip_tags($this->id_utilisateur));
+        $this->contenu = htmlspecialchars(strip_tags($this->contenu));
+
+        $stmt->bindParam(':id_article', $this->id_article);
+        $stmt->bindParam(':id_utilisateur', $this->id_utilisateur);
+        $stmt->bindParam(':contenu', $this->contenu);
+
+        // Exécution de la requête et return true ou false
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
 }
 ?>
