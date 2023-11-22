18efba1e55d4c9e3fdb85f7ee77fdc2031a79c7e
orleans2k17 <orleans2k17@gmail.com>
2023-11-22 14:41:01 +0000

    Vue footer.php

---
+++ Blog/views/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer mt-auto py-3 bg-light">
+        <div class="container text-center">
+            <span class="text-muted">Billet simple pour l'Alaska - un roman de Jean Forteroche</span>
+            
+            <div><a class="nav-link" href="router.php?action=afficherConnexionAdmin">Connexion Administrateur</a></div>
+        </div>
+    </footer>
