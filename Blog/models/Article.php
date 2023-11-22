2f401efd9a5a7167c058268925ce7a59e650d12f
orleans2k17 <orleans2k17@gmail.com>
2023-11-22 10:15:21 +0000

    Classe Article et constructeur

--- Blog/models/Article.php
+++ Blog/models/Article.php
@@ -6,4 +6,19 @@
 //On requiert le fichier pour se connecter à la base de données.
 require_once APP_ROOT .'/config/database.php';
 //On crée une classe Article avec des méthodes qui vont servir à manipuler nos données.
+class Article {
+    //On déclare des variables qu'on va utiliser dans nos méthodes.
+    private $conn;
+    private $table_name = "articles";
+    public $id;
+    public $titre;
+    public $extrait;
+    public $contenu;
+    public $date_publication;
+
+    // Constructeur de classe.
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+}
 ?>
