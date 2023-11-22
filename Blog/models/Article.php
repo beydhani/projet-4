8747e25968d33e48891dd184d201fae91eb7afa7
orleans2k17 <orleans2k17@gmail.com>
2023-11-22 10:14:01 +0000

    Création du modèle article

---
+++ Blog/models/Article.php
@@ -0,0 +1,9 @@
+<?php
+// Article.php
+// C'est le modèle qui va communiquer avec notre base de données pour la table articles.
+// Il va gérer les données et les renvoyer au controleur ArticleController.php
+
+//On requiert le fichier pour se connecter à la base de données.
+require_once APP_ROOT .'/config/database.php';
+//On crée une classe Article avec des méthodes qui vont servir à manipuler nos données.
+?>
