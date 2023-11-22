a9ffccea5905842395823fd3aae0fa64ae4a8d4d
orleans2k17 <orleans2k17@gmail.com>
2023-11-22 10:50:31 +0000

    Constructeur

--- Blog/models/Utilisateur.php
+++ Blog/models/Utilisateur.php
@@ -2,6 +2,7 @@
 // Utilisateur.php
 
 require_once APP_ROOT.'/config/database.php';
+
 class Utilisateur {
     // Connexion à la base de données et nom de la table et propriétés
     private $conn;
@@ -10,5 +11,10 @@ class Utilisateur {
     public $nom_utilisateur;
     public $email;
     public $mot_de_passe;
+
+    // Constructeur avec $db comme connexion à la base de données
+    public function __construct($db) {
+        $this->conn = $db;
+    }
 }
 ?>
