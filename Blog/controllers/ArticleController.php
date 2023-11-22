8a1cd71f84f5628eb4744aea3063e26030f56b07
orleans2k17 <orleans2k17@gmail.com>
2023-11-22 11:09:21 +0000

    Création de la classe et constructeur

---
+++ Blog/controllers/ArticleController.php
@@ -0,0 +1,22 @@
+<?php
+// ArticleController.php
+
+// On start une session si y'en a pas déjà
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+// On require le modele et le fichier de connexion a la database
+require (APP_ROOT .'/models/Article.php');
+require_once (APP_ROOT .'/config/database.php');
+
+class ArticleController {
+    private $db;
+    private $articleModel;
+    public function __construct() {
+        
+        $database = new Database();
+        $this->db = $database->getConnection();
+        $this->articleModel = new Article($this->db);
+    }
+}
+?>
