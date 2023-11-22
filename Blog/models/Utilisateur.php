d70452531ed72333a13b22395c6c6e146dd5b30e
orleans2k17 <orleans2k17@gmail.com>
2023-11-22 10:49:23 +0000

    Création de la classe

--- Blog/models/Utilisateur.php
+++ Blog/models/Utilisateur.php
@@ -2,4 +2,13 @@
 // Utilisateur.php
 
 require_once APP_ROOT.'/config/database.php';
+class Utilisateur {
+    // Connexion à la base de données et nom de la table et propriétés
+    private $conn;
+    private $table_name = "utilisateurs";
+    public $id;
+    public $nom_utilisateur;
+    public $email;
+    public $mot_de_passe;
+}
 ?>
