c3bbde7e0d2154aa9ebdf478e1c88b78d6a9b8c6
orleans2k17 <orleans2k17@gmail.com>
2023-11-22 10:27:38 +0000

    Création du modèle Commentaires

---
+++ Blog/models/Commentaires.php
@@ -0,0 +1,17 @@
+<?php
+// Commentaires.php
+
+require_once APP_ROOT.'/config/database.php';
+
+class Commentaire {
+    // Connexion à la base de données et nom de la table et autres propriétés
+    private $conn;
+    private $table_name = "commentaires";
+    public $id;
+    public $id_article;
+    public $id_utilisateur;
+    public $contenu;
+    public $date_publication;
+    public $signale;
+}
+?>
