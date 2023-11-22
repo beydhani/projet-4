e4dcf6da0b924dcebed58351e59c3cbaacff6d92
orleans2k17 <orleans2k17@gmail.com>
2023-11-22 10:16:37 +0000

    Méthode pour lire tous les articles

--- Blog/models/Article.php
+++ Blog/models/Article.php
@@ -20,5 +20,19 @@ class Article {
     public function __construct($db) {
         $this->conn = $db;
     }
+    // Méthode pour lire tous les articles
+    public function lire() {
+        // Requète SQL
+        $query = "SELECT id, titre, extrait, contenu, date_publication FROM " . $this->table_name . " ORDER BY date_publication DESC";
+
+        // Préparation de la requète avec PDO (méthode prepare de pdo)
+        $stmt = $this->conn->prepare($query);
+
+        // Execution de la requête
+        $stmt->execute();
+
+        // On retourne l'objet PDO $stmt
+        return $stmt;
+    }
 }
 ?>
