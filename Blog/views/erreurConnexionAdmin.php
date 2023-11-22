8ee5d62cebb8efc9fb7a8309f37978ec3a7862eb
orleans2k17 <orleans2k17@gmail.com>
2023-11-22 14:40:09 +0000

    Vue erreurConnexionAdmin.php

---
+++ Blog/views/erreurConnexionAdmin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Erreur de Connexion</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="alert alert-danger" role="alert">
+            Les informations de connexion fournies sont incorrectes. Veuillez réessayer.
+        </div>
+        <a href="/Blog/views/FormulaireConnexionAdmin.php" class="btn btn-primary">Retour au formulaire de connexion</a>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
